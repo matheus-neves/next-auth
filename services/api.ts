f2707651107ad015521549b05f729c0b7d968df2
Matheus Neves <matheusneves1989@gmail.com>
2021-06-29 22:11:57 -0300

    feat: Performing refresh token

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,42 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies';
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${cookies['nextauth.token']}`
+  }
+})
+
+api.interceptors.response.use(response => {
+  return response
+}, (error: AxiosError) => {
+  if (error.response?.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      // refresh token
+      cookies = parseCookies();
+      const { 'nextauth.refreshToken': refreshToken } = cookies;
+
+      api.post('/refresh', {
+        refreshToken
+      }).then(response => {
+
+        setCookie(undefined, 'nextauth.token', response.data.token, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+        setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+
+        api.defaults.headers['Authorization'] = `Bearer ${response.data.token}`;
+      })
+
+    } else {
+      // user logout
+    }
   }
 })
