ad2c9538d85e248340c7057e45ffa98b62a628f5
Matheus Neves <matheusneves1989@gmail.com>
2021-07-01 19:07:39 -0300

    feat: Creating permission hook

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -3,16 +3,26 @@ import { AuthContext } from '../contexts/AuthContext';
 import { withSSRAuth } from '../utils/withSSRAuth';
 import { api } from "../services/apiClient";
 import { setupAPIClient } from "../services/api";
+import { useCan } from '../hooks/useCan'
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
+
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  })
 
   useEffect(() => {
     api.get('/me')
       .then(response => console.log(response))
   }, [])
 
-  return <h1>Dashboard {user?.email}</h1>
+  return (
+    <>
+      <h1>Dashboard {user?.email}</h1>
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  )
 
 }
 
