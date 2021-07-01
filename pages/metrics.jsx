aa320cbbf908126994bb36b794c4008e862d5bbb
Matheus Neves <matheusneves1989@gmail.com>
2021-07-01 20:05:59 -0300

    feat: Permissions validating in server

---
+++ pages/metrics.jsx
@@ -0,0 +1,26 @@
+import { withSSRAuth } from '../utils/withSSRAuth';
+import { setupAPIClient } from "../services/api";
+
+export default function Metrics() {
+
+  return (
+    <>
+      <h1>Metrics</h1>
+    </>
+  )
+
+}
+
+export const getServerSideProps = withSSRAuth(async ctx => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/me');
+  console.log(response.data)
+
+
+  return {
+    props: {}
+  }
+}, {
+  permissions: ['metrics.list'],
+  roles: ['administrator']
+})
